0516db1a60c540e1d1b4364e3c91aa0f5d9e6f37
gaogao <gaoyanshuo9388@yahoo.com>
2023-06-26 10:43:48 +0900

    ngClass ngStyle

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-
+  conditionExpression = 1;
+  colorFlg: boolean = true;
+  attr: string = 'pink';
 }
