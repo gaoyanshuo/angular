762f67a3b4c8260d8e9a9266993591ef152aafb0
gaogao <gaoyanshuo9388@yahoo.com>
2023-06-26 10:45:41 +0900

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+//引入核心模块里面的Component
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //使用这个组件的名称
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularITY';
+  title = 'angularITY'; //定义属性
 }
