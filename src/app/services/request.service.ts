cd3274e00f50de23e93c579065c4cdf86bb4b81e
gaogao <gaoyanshuo9388@yahoo.com>
2023-07-02 17:55:59 +0900

    callback获取异步数据 promise获取异步数据 rxjs获取异步数据

--- src/app/services/request.service.ts
+++ src/app/services/request.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { observeNotification } from 'rxjs/internal/Notification';
 @Injectable({
   providedIn: 'root',
 })
@@ -40,6 +41,90 @@ export class RequestService {
         observable.next(data);
         // observable.error('error');
       }, 2000);
+    });
+  }
+
+  // rxjs多次执行 (promise只能执行一次) example
+
+  getPromiseSetIntervalData() {
+    ///example
+
+    // const myPromise = new Promise((resolve, reject) => {
+    //   // 调用API
+    //   fetch('https://api.example.com/data')
+    //     .then(response => {
+    //       // 处理响应数据
+    //       if (response.ok) {
+    //         return response.json();
+    //       } else {
+    //         throw new Error('API请求失败');
+    //       }
+    //     })
+    //     .then(data => {
+    //       // 成功时调用resolve()，传递数据
+    //       resolve(data);
+    //     })
+    //     .catch(error => {
+    //       // 失败时调用reject()，传递错误信息
+    //       reject(error);
+    //     });
+    // });
+
+    // 调用函数
+    //   myPromise
+    // .then(data => {
+    //   // 处理成功的结果数据
+    //   console.log(data);
+    // })
+    // .catch(error => {
+    //   // 处理错误信息
+    //   console.log(error);
+    // });
+
+    return new Promise((resolve, reject) => {
+      let num: number = 0;
+      setInterval(() => {
+        num++;
+        resolve(num);
+      }, 1000);
+    });
+  }
+
+  getRxjsSetIntervalData() {
+    /// example
+
+    // const myObservable = new Observable(observer => {
+    //   // 调用API
+    //   fetch('https://api.example.com/data')
+    //     .then(response => {
+    //       // 处理响应数据
+    //       if (response.ok) {
+    //         return response.json();
+    //       } else {
+    //         throw new Error('API请求失败');
+    //       }
+    //     })
+    //     .then(data => {
+    //       // 发出数据
+    //       observer.next(data);
+    //       // 完成Observable
+    //       observer.complete();
+    //     })
+    //     .catch(error => {
+    //       // 发生错误
+    //       observer.error(error);
+    //     });
+    // });
+
+    return new Observable((observable) => {
+      let num: number = 0;
+      let id: any = '';
+      id = setInterval(() => {
+        num++;
+        if (num > 2) clearInterval(id);
+        observable.next(num);
+        observable.next(num + 10086);
+      }, 1000);
     });
   }
 }
